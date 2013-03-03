d9f1c742b875b1b71e5a877d255ff910ea45e200
Periklis Ntanasis <pntanasis@gmail.com>
2013-03-03 23:10:08 +0200

    camera box isn't displayed in the selection list if camera not available

--- src/mmb/foss/aueb/icong/SelectionActivity.java
+++ src/mmb/foss/aueb/icong/SelectionActivity.java
@@ -1,7 +1,10 @@
 package mmb.foss.aueb.icong;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,16 +26,9 @@ public class SelectionActivity extends Activity {
 			chanMergeBox, forkBox, valueEntryBox, sliderBox, blurBox, edgesBox,
 			invertBox, clipBox, rgb2hsvBox, hsv2rgbBox;
 	// fixed size of entries, no need for ArrayList or other dynamic data set
-	private ImageView[] listImages = new ImageView[15];
-	private String[] imageNames = { "ImageFile", "Camera", "Screen",
-			"Channel Split", "Mix", "Channel Merge", "Fork", "Value Entry",
-			"Slider", "Blur", "Edges", "Invert", "Clip", "RGB to HSV",
-			"HSV to RGB" };
-	private String[] imageDescriptions = { "Gets/sets input/output to file",
-			"Gets input from the Camera", "Outputs the result to the screen",
-			"Splits the color channel", "Mixes", "Merges the color channel",
-			"Forks", "Inputs usr defined values", "Inputs user defined values",
-			"Blurs", "Edges", "Inverts the image", "Clips", "", "" };
+	private ArrayList<ImageView> listImages = new ArrayList<ImageView>();
+	private ArrayList<String> imageNames = new ArrayList<String>();
+	private ArrayList<String> imageDescriptions = new ArrayList<String>();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -40,49 +36,85 @@ public class SelectionActivity extends Activity {
 		setContentView(R.layout.selection_layout);
 		imagefileBox = new ImageView(getBaseContext());
 		imagefileBox.setImageResource(R.drawable.image_file);
-		listImages[0] = imagefileBox;
-		cameraBox = new ImageView(getBaseContext());
-		cameraBox.setImageResource(R.drawable.camera);
-		listImages[1] = cameraBox;
+		listImages.add(imagefileBox);
+		imageNames.add("ImageFile");
+		imageDescriptions.add("Gets/sets input/output to file");
+		try {
+			Camera camera = Camera.open();
+			camera.release();
+			cameraBox = new ImageView(getBaseContext());
+			cameraBox.setImageResource(R.drawable.camera);
+			listImages.add(cameraBox);
+			imageNames.add("Camera");
+			imageDescriptions.add("Gets/sets input/output to file");
+		} catch (Exception e) {
+			// no camera available
+		}
 		screenBox = new ImageView(getBaseContext());
 		screenBox.setImageResource(R.drawable.screen);
-		listImages[2] = screenBox;
+		listImages.add(screenBox);
+		imageNames.add("Screen");
+		imageDescriptions.add("Outputs the result to the screen");
 		chanSplitBox = new ImageView(getBaseContext());
 		chanSplitBox.setImageResource(R.drawable.chan_split);
-		listImages[3] = chanSplitBox;
+		listImages.add(chanSplitBox);
+		imageNames.add("Channel Split");
+		imageDescriptions.add("Splits the color channel");
 		mixBox = new ImageView(getBaseContext());
 		mixBox.setImageResource(R.drawable.mix);
-		listImages[4] = mixBox;
+		listImages.add(mixBox);
+		imageNames.add("Mix");
+		imageDescriptions.add("Mixes");
 		chanMergeBox = new ImageView(getBaseContext());
 		chanMergeBox.setImageResource(R.drawable.chan_merge);
-		listImages[5] = chanMergeBox;
+		listImages.add(chanMergeBox);
+		imageNames.add("Channel Merge");
+		imageDescriptions.add("Merges the color channel");
 		forkBox = new ImageView(getBaseContext());
 		forkBox.setImageResource(R.drawable.fork);
-		listImages[6] = forkBox;
+		listImages.add(forkBox);
+		imageNames.add("Fork");
+		imageDescriptions.add("Forks");
 		valueEntryBox = new ImageView(getBaseContext());
 		valueEntryBox.setImageResource(R.drawable.value_entry_preview);
-		listImages[7] = valueEntryBox;
+		listImages.add(valueEntryBox);
+		imageNames.add("Value Entry");
+		imageDescriptions.add("Inputs usr defined values");
 		sliderBox = new ImageView(getBaseContext());
 		sliderBox.setImageResource(R.drawable.slider_preview);
-		listImages[8] = sliderBox;
+		listImages.add(sliderBox);
+		imageNames.add("Slider");
+		imageDescriptions.add("Inputs user defined values");
 		blurBox = new ImageView(getBaseContext());
 		blurBox.setImageResource(R.drawable.blur);
-		listImages[9] = blurBox;
+		listImages.add(blurBox);
+		imageNames.add("Blur");
+		imageDescriptions.add("Blurs");
 		edgesBox = new ImageView(getBaseContext());
 		edgesBox.setImageResource(R.drawable.edges);
-		listImages[10] = edgesBox;
+		listImages.add(edgesBox);
+		imageNames.add("Edges");
+		imageDescriptions.add("Edges");
 		invertBox = new ImageView(getBaseContext());
 		invertBox.setImageResource(R.drawable.invert);
-		listImages[11] = invertBox;
+		listImages.add(invertBox);
+		imageNames.add("Invert");
+		imageDescriptions.add("Inverts the image");
 		clipBox = new ImageView(getBaseContext());
 		clipBox.setImageResource(R.drawable.clip);
-		listImages[12] = clipBox;
+		listImages.add(clipBox);
+		imageNames.add("Clip");
+		imageDescriptions.add("Clips");
 		rgb2hsvBox = new ImageView(getBaseContext());
 		rgb2hsvBox.setImageResource(R.drawable.rgb2hsv);
-		listImages[13] = rgb2hsvBox;
+		listImages.add(rgb2hsvBox);
+		imageNames.add("RGB to HSV");
+		imageDescriptions.add("");
 		hsv2rgbBox = new ImageView(getBaseContext());
 		hsv2rgbBox.setImageResource(R.drawable.hsv2rgb);
-		listImages[14] = hsv2rgbBox;
+		listImages.add(hsv2rgbBox);
+		imageNames.add("HSV to RGB");
+		imageDescriptions.add("");
 
 		ListView listView = (ListView) findViewById(R.id.selectionlist);
 		SelectionAdapter adapter = new SelectionAdapter(this, listImages,
@@ -96,7 +128,6 @@ public class SelectionActivity extends Activity {
 					long arg3) {
 				// TODO Auto-generated method stub
 				Intent selectedBox = new Intent();
-				Log.e("yaw", "" + Uri.parse("" + arg2));
 				selectedBox.setData(Uri.parse("" + arg2));
 				setResult(RESULT_OK, selectedBox);
 				finish();
