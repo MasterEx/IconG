66b1fe2470a2ec86718c245150abbccb1e2532aa
Periklis Ntanasis <pntanasis@gmail.com>
2013-03-04 04:47:54 +0200

    save image method

--- src/mmb/foss/aueb/icong/dialogs/FileManager.java
+++ src/mmb/foss/aueb/icong/dialogs/FileManager.java
@@ -1,13 +1,16 @@
 package mmb.foss.aueb.icong.dialogs;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 
 public class FileManager 
-{
-	
+{	
 	private Uri[] Urls;
 	static String[] files = null ;
 	static String old_dir_initialized = null ;
@@ -35,5 +38,23 @@ public class FileManager
 			Urls[i] = Uri.parse(rootDir+File.separator+files[i]);
 		}
 		return Urls;
+	}
+	
+	public void saveBitmap(Bitmap bitmap, String filename, Bitmap.CompressFormat compressType) {
+		if(rootDir.exists() && Environment.getExternalStorageState() != null) {
+			File destinationFile = new File(rootDir,filename); 
+			try {
+				FileOutputStream fos = new FileOutputStream(destinationFile);
+				bitmap.compress(compressType, 100, fos);
+				fos.flush();
+				fos.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
