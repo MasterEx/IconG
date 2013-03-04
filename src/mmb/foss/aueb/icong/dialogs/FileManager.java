f37efe6f4fcaf55076e2e2922aae18b8f397fc30
Periklis Ntanasis <pntanasis@gmail.com>
2013-03-04 13:05:00 +0200

    This may work with both sdcard and internal storage mounted as sdcard but is hackish and unreliable

--- src/mmb/foss/aueb/icong/dialogs/FileManager.java
+++ src/mmb/foss/aueb/icong/dialogs/FileManager.java
@@ -4,45 +4,66 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 
-public class FileManager 
-{	
+public class FileManager {
 	private Uri[] Urls;
-	static String[] files = null ;
-	static String old_dir_initialized = null ;
+	static String[] files = null;
+	static String old_dir_initialized = null;
 	private final String appDir = "icong";
-	private File rootDir;
+	private File rootDir = null;
 
 	public FileManager() {
-		rootDir = new File(Environment.getExternalStorageDirectory(),appDir);
-		if(!rootDir.exists() && Environment.getExternalStorageState() != null)
-			rootDir.mkdir();	
-	}
-	
-	public String[] getFiles() {
-		if(Environment.getExternalStorageState() != null) {
-			if(rootDir.exists() && rootDir.canRead())
-				files = rootDir.list();
+		File mnt = new File("/" + File.separator + "mnt");
+		rootDir = new File(new File(mnt, "sdcard"), appDir);
+
+		if (rootDir.exists() && rootDir.isDirectory() && rootDir.canWrite()
+				&& rootDir.canRead()) {
+			rootDir = new File(Environment.getExternalStorageDirectory(),
+					appDir);
+			rootDir.mkdirs();
+		} else {
+			for (String dir : mnt.list()) {
+				if (dir.contains("sdcard") && !dir.equals("sdcard")) {
+					rootDir = new File(new File(mnt, dir), appDir);
+					rootDir.mkdirs();
+					break;
+				}
+			}
 		}
-		return files;
+	}
+
+	public String[] getFiles() {
+		ArrayList<String> files = new ArrayList<String>();
+		if (rootDir != null) {
+			for (String file : rootDir.list())
+				if (file.endsWith(".png") || file.endsWith(".PNG")
+						|| file.endsWith(".gif") || file.endsWith(".GIF")
+						|| file.endsWith(".jpg") || file.endsWith(".JPG")
+						|| file.endsWith("jpeg") || file.endsWith(".JPEG"))
+					files.add(file);
+		}
+		this.files = files.toArray(new String[0]);
+		return this.files;
 	}
 
 	public Uri[] getAllImagesFromDir() {
 		// TODO Auto-generated method stub
 		Urls = new Uri[files.length];
-		for(int i=0;i<files.length;i++) {
-			Urls[i] = Uri.parse(rootDir+File.separator+files[i]);
+		for (int i = 0; i < files.length; i++) {
+			Urls[i] = Uri.parse(rootDir + File.separator + files[i]);
 		}
 		return Urls;
 	}
-	
-	public void saveBitmap(Bitmap bitmap, String filename, Bitmap.CompressFormat compressType) {
-		if(rootDir.exists() && Environment.getExternalStorageState() != null) {
-			File destinationFile = new File(rootDir,filename); 
+
+	public void saveBitmap(Bitmap bitmap, String filename,
+			Bitmap.CompressFormat compressType) {
+		if (rootDir.exists()) {
+			File destinationFile = new File(rootDir, filename);
 			try {
 				FileOutputStream fos = new FileOutputStream(destinationFile);
 				bitmap.compress(compressType, 100, fos);
